f2acda37960bff12578adaa23aa59a43d61c3498
Andrew Beckerman <andrew.beckerman@gmail.com>
2014-04-07 13:42:58 +0100

    First Upload of Codes

---
+++ psb_2trait.R
@@ -0,0 +1,162 @@
+
+
+###############################################################################################
+###############################################################################################
+# collect.matrices: Function to collect relevant matrices from the MCMCglmm models
+###############################################################################################
+###############################################################################################
+
+collect.matrices<-function(model,no.traits=2){
+	
+	if (!inherits(model, "MCMCglmm")) 
+        stop("use only with \"MCMCglmm\" objects")
+	
+	# get dimension of Gcov
+	GCtest<-length(as.numeric(summary(model)$Gcovariances[,1]))
+	
+	#Gmatrix calculation	
+	ifelse(GCtest!=0, GCmat<-matrix(posterior.mode(model$VCV[,1:GCtest]),sqrt(GCtest),sqrt(GCtest),byrow=TRUE), GCmat <- 0)
+	
+	# Residual P (E) matrix calculation
+	ifelse(GCtest==0, RCmat<-matrix(posterior.mode(model$VCV),no.traits,no.traits,byrow=TRUE), RCmat<-0) # we ignore this if method = G.... only valid when method = P
+	
+	# Phen values for centroids
+	P<-as.numeric(summary(model)$solutions[,1])
+
+	# make generic trait names
+	ll<-length(as.data.frame(model$Sol))
+	nn<-paste("trait",1:no.traits)
+	
+	return(list(GenCov=GCmat,RCov=RCmat,Locate=P,Names=nn))
+	}
+
+
+########################################################################################
+## PLOTSUBSPACE 2 - psb2
+########################################################################################
+
+psb_2trait <- function (model1,model2,no.traits=2,method=c("G","P"),corr = FALSE, shadeCA = TRUE, shadeCB = TRUE, 
+    axes.lab = FALSE, xlims=NULL, ylims=NULL, xlab="Trait1", ylab="Trait2",...) 
+{
+		if (require(ellipse) == FALSE)
+        stop("ellipse library not loaded")
+		
+		if (!inherits(model1, "MCMCglmm")) 
+        stop("use only with \"MCMCglmm\" objects")
+		
+	
+	# use collect matrices function to get all cov mats and location paramters
+	# specify shrinkage use here
+
+	e1mat<-collect.matrices(model1)
+	e2mat<-collect.matrices(model2)		
+
+	# Follow Jarrod methods for subspace plotting - G or R or P or I   
+	#-------------------------------------------------------------------------#
+	## This function is modified from Jarrod Hadfields plotsubspace function ##
+	##                         MCMCglmm package                              ##
+	#-------------------------------------------------------------------------#
+	
+	#----------------------------- G Method method ---------------------------#
+	if(method=="G"){
+		if(is.null(dim(e1mat$GenCov)))
+    		stop("Method G inappropriate - No Gmatrix calculated in model")
+    	
+    	if (length(as.numeric(summary(model1)$Gcovariances[,1]))<2) 
+        	stop("Random effects not speficied to estiamte VCV")
+    		
+    		
+	if(method=="G"){
+		CA<-e1mat$GenCov
+				dimnames(CA)<-list(e1mat$Names,e1mat$Names)
+		CB<-e2mat$GenCov
+			dimnames(CB)<-list(e1mat$Names,e1mat$Names)
+		}
+	
+	if(method=="E"){
+		CA<-e1mat$RCov
+			dimnames(CA)<-list(e1mat$Names,e1mat$Names)
+		CB<-e1mat$RCov
+			dimnames(CB)<-list(e1mat$Names,e1mat$Names)
+		}
+	
+	es1<-eigen(CA)
+	es2<-eigen(CB)
+	varA<-sum(es1$values)
+	varB<-sum(es2$values)
+	
+	plot(ellipse(CA),type="l",asp=1)
+		
+		Val <- es1$values
+		Vec <- es1$vectors 
+		v1 <- sqrt(Val[1]*qchisq(0.95,2))*Vec[,1]
+		v2 <- sqrt(Val[2]*qchisq(0.95,2))*Vec[,2]
+		segments(-v1[1],-v1[2],v1[1],v1[2]) 
+		segments(-v2[1],-v2[2],v2[1],v2[2]) 	
+		
+	points(ellipse(CB),type="l",col="grey40",lty=2)
+		Val <- es2$values
+		Vec <- es2$vectors 
+		v1 <- sqrt(Val[1]*qchisq(0.95,2))*Vec[,1]
+		v2 <- sqrt(Val[2]*qchisq(0.95,2))*Vec[,2]
+		segments(-v1[1],-v1[2],v1[1],v1[2],col="grey40",lty=2) 
+		segments(-v2[1],-v2[2],v2[1],v2[2],col="grey40",lty=2) 	
+	
+	cat("\n")
+	cat("Variance in Each Environment (sum Eigenvalues)","\n")
+	props<-matrix(c(varA,varB),1,2,dimnames=list(c("Proportion"),c("Env 1","Env2")))
+	print(props)	
+   }#----------------------- P Method ----------------------------------------------------------------------------------------#
+	if(method=="P"){
+	
+	if(!is.null(dim(e1mat$GenCov)))
+		stop("Do not use P method with models estimating Genetic Covariance")
+	if (require(ellipse) == FALSE)
+        stop("ellipse library not loaded")
+	if (!inherits(model1, "MCMCglmm")) 
+        stop("use only with \"MCMCglmm\" objects")
+	if (length(as.numeric(summary(model1)$Rcovariances[,1]))<2) 
+        stop("RCov not speficied to estiamte VCV")
+		
+	CA<-e1mat$RCov
+		dimnames(CA)<-list(e1mat$Names,e1mat$Names)
+	CB<-e2mat$RCov
+		dimnames(CB)<-list(e2mat$Names,e2mat$names)
+	
+	ctrA<-e1mat$Locate
+	ctrB<-e2mat$Locate
+	
+	es1<-eigen(CA)
+	es2<-eigen(CB)
+	varA<-sum(es1$values)
+	varB<-sum(es2$values)
+	
+	
+	plot(ellipse(CA,centre=ctrA),type="l",asp=1, xlim=xlims, ylim=ylims, xlab=xlab, ylab=ylab)
+	points(ctrA[1],ctrA[2],pch=19)
+		
+		Val <- es1$values
+		Vec <- es1$vectors 
+		v1 <- (sqrt(Val[1]*qchisq(0.95,2))*Vec[,1])
+		v2 <- (sqrt(Val[2]*qchisq(0.95,2))*Vec[,2])
+		segments(-v1[1]+ctrA[1],-v1[2]+ctrA[2],v1[1]+ctrA[1],v1[2]+ctrA[2]) 
+		segments(-v2[1]+ctrA[1],-v2[2]+ctrA[2],v2[1]+ctrA[1],v2[2]+ctrA[2]) 	
+		
+	points(ellipse(CB,centre=ctrB),type="l",col="grey40",lty=2)
+	points(ctrB[1],ctrB[2],pch=19,col="grey40")
+	
+		Val <- es2$values
+		Vec <- es2$vectors 
+		v1 <- (sqrt(Val[1]*qchisq(0.95,2))*Vec[,1])
+		v2 <- (sqrt(Val[2]*qchisq(0.95,2))*Vec[,2])
+		segments(-v1[1]+ctrB[1],-v1[2]+ctrB[2],v1[1]+ctrB[1],v1[2]+ctrB[2],col="grey40") 
+		segments(-v2[1]+ctrB[1],-v2[2]+ctrB[2],v2[1]+ctrB[1],v2[2]+ctrB[2],col="grey40") 
+	
+	
+	}
+}
+
+########################################################################################
+#----------------------- END PLOT METHODS ---------------------------------------------#
+########################################################################################
+
---
+++ tensor_function.R
@@ -0,0 +1,287 @@
+#### -----------------------------------------------------------------------------------------
+#### Begin tensor.stats FUNCTION
+#### By Matt Robinson and Andrew Beckerman
+#### -----------------------------------------------------------------------------------------
+
+tensor.stats<-function(models,no.traits,no.trt){
+	if (require(MCMCglmm) == FALSE)
+		stop("MCMCglmm not loaded")
+	if (require(mvtnorm) == FALSE)
+        stop("mvtnorm not loaded")
+	if (!inherits(models[[1]], "MCMCglmm")) 
+        stop("use only with \"MCMCglmm\" objects")
+    if(no.trt!=length(models))
+        stop("Missing a model for some treatments")
+	if(dim(models[[1]]$VCV)[1]!=1000)
+		stop("please re-run your models to get a joint posterior of 1000 samples")
+	
+	n<-no.traits
+	m<-no.trt
+	d<-(n*(n+1)/2)+1 # tensor dimension (when is it +1????)
+		
+	wrk.set<-vector("list", m)
+	
+	for(i in 1:m){
+		wrk.set[[i]]<-data.frame(models[[i]]$VCV[,1:(n)^2])
+	}
+
+# Collection Zones
+	sum_eigvals<-matrix(0,1000,d)
+	sum_projmat<-vector("list",1000)
+	sum_eigtenvecs<-vector("list",1000)
+	sum_eigtenvals<-vector("list",1000)
+
+# Set up tensor shite lists
+	proj_mat<-vector("list",(d-1)) # list has n*(n+1)/2) matrices (rows of projmat) each m = trt columns
+	for (i in 1:(d-1)){
+		proj_mat[[i]]<-matrix(0,1000,m)
+	}
+
+	eigten_vec<-vector("list",d) # list has n*(n+1)/2)+1 matrices each n = trait columns 
+	for (i in 1:(d-1)){
+		eigten_vec[[i]]<-matrix(0,1000,n)
+	}
+
+	eigten_vals<-vector("list",d) # list has n*(n+1)/2)+1 matrices each n = trait columns 
+	for (i in 1:(d-1)){
+		eigten_vals[[i]]<-matrix(0,1000,n)
+	}
+
+# MASTER LOOPING OVER 1000
+	for (z in 1:1000){	
+
+########################################################################################################
+## 1. Stack the matrices from each MCMC model into one file where ncol = number of traits and nrow = n traits * i environments
+		Gs<-list()
+			for(i in 1:m){
+			Gs[[i]]<-matrix(as.numeric(wrk.set[[i]][z,][,1:(n)^2]),n,n)
+			}
+	
+		Gmatrices<-matrix(unlist(Gs),m*n,n,byrow=TRUE)
+
+########################################################################################################
+## 2. Calculate the matrix representation of the covariance tensor (S) 
+
+		covcov<-matrix(ncol=n*(n+1)/2,nrow=n*(n+1)/2) # matrix representation of covariance tensor  "S"
+
+# upper left quadrant of S
+		for (i in 1:n){
+			for (j in 1:n){
+			    covcov[i,j]=cov(Gmatrices[seq(i,n*m,n),i],Gmatrices[seq(j,n*m,n),j])
+			}
+		}
+
+# lower left and upper right quadrants of S
+		for (k in 1:n){
+			count<-1
+		  	for (i in 1:(n-1)){
+    			for (j in (i+1):n){
+		      		covcov[k,count+n]=sqrt(2)*cov(Gmatrices[seq(k,n*m,n),k],Gmatrices[seq(i,n*m,n),j])
+      				covcov[count+n,k]=sqrt(2)*cov(Gmatrices[seq(k,n*m,n),k],Gmatrices[seq(i,n*m,n),j])
+				count=count+1
+				}
+			}
+		}
+
+# lower right quadrant of S
+		countx<-1
+		county<-1
+		for (k in 1:(n-1)){
+			for (l in (k+1):n){
+				for (i in 1:(n-1)){
+					for (j in (i+1):n){
+						covcov[countx+n,county+n]=2*cov(Gmatrices[seq(k,n*m,n),l],Gmatrices[seq(i,n*m,n),j])
+						covcov[county+n,countx+n]=2*cov(Gmatrices[seq(k,n*m,n),l],Gmatrices[seq(i,n*m,n),j])
+						county=county+1}
+        			}
+			countx=countx+1
+			county=1
+			}
+		}
+
+		covcov<-as.data.frame(covcov)
+
+########################################################################################################
+## 3. Take eigenvectors and eigenvalues of covariance tensor S, 
+## and then for each eigenvector of S construct and eigentensor,
+## then take eigenvectors and eigenvalues of each eigentensor
+
+		eigvecs<-eigen(covcov)$vectors
+		eigvals<-eigen(covcov)$values
+		eigten<-matrix(nrow=n*n*(n+1)/2,ncol=n)
+
+		for (eval in 1:(n*(n+1)/2)){ 								#number of eigenvalues of covariance tensor
+			for (i in 1:n){ 										#constructing an n*n eigentensor from each eigenvector of S matrix
+    			eigten[i+n*(eval-1),i]<-eigvecs[i,eval] 				#this loop calculates the diagonal
+			}
+
+			count<-1
+				for (i in 1:(n-1)){
+					for (j in (i+1):n){  						eigten[i+n*(eval-1),j]<-1/sqrt(2)*eigvecs[count+n,eval]
+						eigten[j+n*(eval-1),i]<-1/sqrt(2)*eigvecs[count+n,eval]
+					count<-count+1
+					}
+				}
+		}
+
+# storage matrices of eigentensor eigenvalues and eigenvectors
+		eigtenvecs<-matrix(nrow=n*n*(n+1)/2, ncol=n)
+		eigtenvals<-matrix(nrow=n*n*(n+1)/2, ncol=1)
+
+		for (i in 1:(n*(n+1)/2)){
+			eigtenvecs[((i-1)*n+1):(i*n),]<-t(eigen(eigten[((i-1)*n+1):(i*n),])$vectors) # eigenvectors in rows as in Excel
+			eigtenvals[((i-1)*n+1):(i*n),1]<-eigen(eigten[((i-1)*n+1):(i*n),])$values
+		}
+
+########################################################################################################\
+## 4. Characterise the genetic variance in a single trait combination (y) by projecting y through the G matrix using t(y)%*%G%*%y\
+
+neigten=n*(n+1)/2
+
+projmat<-matrix(ncol=m,nrow=neigten) #matrix containing dot product for each eigentensor with each population delta G
+# Cj,k values for population 1 are in the first column, etc.
+
+for (neigs in 1:neigten){
+ for (npop in 1:(m)){
+  inside=0
+  for (i in 1:n){
+   for (j in 1:n){
+    term=Gmatrices[(npop-1)*n+i,j]*eigten[(neigs-1)*n+i,j]
+    inside=inside+term
+					}
+				}
+  		projmat[neigs,npop]=inside
+	}
+}
+
+#  ADJUST 1-2 level collections here and at start to be lists..... to make generic
+
+		# Summary of eigenvalues
+		sum_eigvals[z,1:(d-1)]<-eigvals
+		sum_eigvals[z,d]<-sum(eigvals)
+
+		# Projection Matrix Collection
+		sum_projmat[[z]]<-projmat
+
+		# summary vectors and values from proj matrix
+		sum_eigtenvecs[[z]]<-eigtenvecs
+		sum_eigtenvals[[z]]<-eigtenvals
+
+		# allocate details
+		for(i in 1:(d-1)){
+			proj_mat[[i]][z,]<-projmat[i,]
+			}
+
+		for(i in 1:(d-1)){
+			eigten_vec[[i]][z,]<-eigtenvecs[i,]
+			}
+		
+		code<-rep(1:(n*m), each=n)
+		for(i in 1:(d-1)){
+			eigten_vals[[i]][z,]<-eigtenvals[code==i]
+			}
+	}
+
+	tens.out<-list(
+		sum_eigvals,
+		sum_projmat,
+		sum_eigtenvecs,
+		sum_eigtenvals,
+		proj_mat,
+		eigten_vec,
+		eigten_vals,
+		tr.names=rownames(summary(models[[1]])$solutions))
+	
+	return(tens.out)
+}
+#### -----------------------------------------------------------------------------------------
+#### END tensor.stats FUNCTION
+#### -----------------------------------------------------------------------------------------
+
+
+#### -----------------------------------------------------------------------------------------
+#### Begin tensor.analyse FUNCTION
+#### By Matt Robinson and Andrew Beckerman
+#### -----------------------------------------------------------------------------------------
+
+tensor.analyse<-function(dd,how.many.tensors=1, no.traits = 2){
+	n<-no.traits
+	if(length(dd)!=8)
+	stop("tensor stats function failed to produce correct info")
+cat("\n***********************************\n")
+cat("*** Core Tensor Analysis Output ***\n")
+cat("***********************************\n")	
+cat("\n --- 95% CI of Variance Explained by each tensor \n")
+	tmp<-c()
+	for(i in 1:how.many.tensors){
+		cat("\n Axis ",i," \n")
+		tmp[i]<-posterior.mode(as.mcmc(dd[[1]][,i]/dd[[1]][,((n*(n+1)/2)+1)]))
+		print(
+			cbind(
+				MODE = posterior.mode(as.mcmc(dd[[1]][,i]/dd[[1]][,((n*(n+1)/2)+1)])),
+				CI = HPDinterval(as.mcmc(dd[[1]][,i]/dd[[1]][,((n*(n+1)/2)+1)]))
+				)
+			)
+	}
+cat("\nThe number of significant tensors is ~ where the index of this cumsum = 1 \n")
+cat("Consider adding another tensor dimension if it has not reached 1 \n")
+
+print(cumsum(tmp))
+
+cat("\n *For the next two derived stats, know your traits:\n")
+print(dd[[8]])
+
+cat("\n --- For the significant eigentensors, the Directional Change in Variance of each trait for each vector")
+	for(i in 1:how.many.tensors){
+		cat("\n Axis ",i," \n")
+		print(
+			cbind(
+			MODE = posterior.mode(as.mcmc(dd[[7]][[i]][,1:no.traits])), 
+			CI = HPDinterval(as.mcmc(dd[[7]][[i]][,1:no.traits]))
+				)
+			)
+	}	
+	
+cat("\n\n --- The Proportion of Variance of each eigentensor explianed by each trait \n")
+for(i in 1:how.many.tensors){
+		cat("\n Axis ",i," \n")
+		tmp<-dd[[7]][[i]]
+		tdim<-dim(tmp)[2]
+		pvar<- matrix(0,1000,no.traits)
+		# calc tot var across all traits
+		# should be 1 for 1st and <1 for next
+		tot<-apply(tmp,1,function(x) sum(x^2))
+		# for each trait
+			for(j in 1:1000){
+				for(k in 1:no.traits){
+					pvar[j,k]<-tmp[j,k]^2/tot[j]	
+					}
+				}
+		print(
+			cbind(
+				MODE = posterior.mode(as.mcmc(pvar[,1:no.traits])), 
+				CI = HPDinterval(as.mcmc(pvar[,1:no.traits]))
+				)
+			)
+	}
+
+cat("\n *For the next stats, recall the order of your treat/envs in the model list* \n")
+
+cat("\n --- Genetic variance of each of GEI for each env")
+	for(i in 1:how.many.tensors){
+		cat("\n Axis ",i," \n")
+		tdim<-dim(dd[[5]][[i]])[2]
+		print(
+			cbind(
+				Mode = posterior.mode(as.mcmc(sqrt(dd[[5]][[i]][,1:tdim]^2))),
+				CI = HPDinterval(as.mcmc(sqrt(dd[[5]][[i]][,1:tdim]^2)))
+				)
+			)
+	}
+
+cat("\n\n *** Extra Output ***")
+cat("\n 95% CI of Eigenvalues of the Eigentensors \n")
+	print(posterior.mode(as.mcmc(dd[[1]][,1:((n*(n+1)/2)+1)])))
+	print(HPDinterval(as.mcmc(dd[[1]][,1:((n*(n+1)/2)+1)])))
+
+}
